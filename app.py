7c68a2bd18b247aa6378e187d101c4ae7a9d0078
Leyes Gonzalo Matías <71945620+Gonza-L@users.noreply.github.com>
2024-02-15 19:41:04 -0300

    Update headers

--- app.py
+++ app.py
@@ -3,7 +3,21 @@ import pandas as pd
 import requests
 from datetime import datetime, timedelta
 
-headers = {"User-Agent": "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:122.0) Gecko/20100101 Firefox/122.0"}
+# Define headers
+headers = {
+    "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8",
+    "Accept-Encoding": "gzip, deflate, br",
+    "Accept-Language": "en-US,en;q=0.5",
+    "Connection": "keep-alive",
+    "Host": "api.nasdaq.com",
+    "Sec-Fetch-Dest": "document",
+    "Sec-Fetch-Mode": "navigate",
+    "Sec-Fetch-Site": "none",
+    "Sec-Fetch-User": "?1",
+    "TE": "trailers",
+    "Upgrade-Insecure-Requests": "1",
+    "User-Agent": "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:122.0) Gecko/20100101 Firefox/122.0"
+}
 
 # Function to make a GET request
 def make_request(url, headers):
