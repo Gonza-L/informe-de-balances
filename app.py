00ee0e18d9618635908fc361510c6d5b232efd44
Leyes Gonzalo Matías <71945620+Gonza-L@users.noreply.github.com>
2024-02-16 03:12:09 -0300

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import pandas as pd
 import requests
 from datetime import datetime, timedelta
 from requests.adapters import HTTPAdapter
-from requests.packages.urllib3.util.retry import Retry
+from urllib3.util.retry import Retry
 import concurrent.futures
 
 # Function to make a GET request with retry mechanism
