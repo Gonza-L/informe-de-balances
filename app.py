cb93e33ebe21664ab7621dc115f1e81a5fb12a75
Leyes Gonzalo Matías <71945620+Gonza-L@users.noreply.github.com>
2024-02-16 03:07:31 -0300

    Update app.py

--- app.py
+++ app.py
@@ -2,12 +2,29 @@ import streamlit as st
 import pandas as pd
 import requests
 from datetime import datetime, timedelta
+from requests.adapters import HTTPAdapter
+from requests.packages.urllib3.util.retry import Retry
 import concurrent.futures
 
-# Function to make a GET request
-def make_request(url):
-    headers = {"User-Agent": "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:122.0) Gecko/20100101 Firefox/122.0"}
-    return requests.get(url, headers=headers)
+# Function to make a GET request with retry mechanism
+def make_request_with_retry(url):
+    session = requests.Session()
+    retry_strategy = Retry(
+        total=69,
+        status_forcelist=[403],  # Retry on specific status codes
+        method_whitelist=["GET"],
+        backoff_factor=1
+    )
+    adapter = HTTPAdapter(max_retries=retry_strategy)
+    session.mount("https://", adapter)
+    session.mount("http://", adapter)
+    
+    try:
+        response = session.get(url)
+        response.raise_for_status()  # Raise HTTPError for bad status codes
+        return response
+    except requests.RequestException as e:
+        return None
 
 # Function to parse the JSON response
 def parse_response(response):
@@ -35,7 +52,7 @@ def make_historical_data_request(symbol, date_reported, time_slot):
     from_date = (datetime.strptime(date_reported, '%m/%d/%Y') + timedelta(days=1 if time_slot == 'time-after-hours' else 0)).strftime('%Y-%m-%d')
     
     url = f"https://api.nasdaq.com/api/quote/{symbol}/historical?assetclass=stocks&fromdate={from_date}&limit=365"
-    return make_request(url)
+    return make_request_with_retry(url)
 
 # Function to fetch variances
 def fetch_variances(time_slot, symbol, date_reported_list):
@@ -59,7 +76,7 @@ def fetch_variances(time_slot, symbol, date_reported_list):
 # Function to fetch data for selected date
 def fetch_data(selected_date):
     url = f"https://api.nasdaq.com/api/calendar/earnings?date={selected_date.strftime('%Y-%m-%d')}"
-    response = make_request(url)
+    response = make_request_with_retry(url)
     
     if response.status_code != 200:
         st.error(f"Request failed with status code: {response.status_code}")
@@ -79,7 +96,7 @@ def display_progress(companies_list):
     
     with st.progress(0):
         for idx, item in enumerate(companies_list, start=1):
-            date_reported_list = extract_date_reported(parse_response(make_request(f"https://api.nasdaq.com/api/company/{item['symbol']}/earnings-surprise")))
+            date_reported_list = extract_date_reported(parse_response(make_request_with_retry(f"https://api.nasdaq.com/api/company/{item['symbol']}/earnings-surprise")))
             if date_reported_list:
                 variances = fetch_variances(item['time'], item['symbol'], date_reported_list)
                 if variances:
